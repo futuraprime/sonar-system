0e4bcbf3e97c90d1ad4e2853cae69ffc80cbd7de
Evan Hensleigh <futuraprime@gmail.com>
2014-11-18 22:59:12 -0500

    Images in

---
+++ js/main.js
@@ -0,0 +1,38 @@
+var s = Snap('#interactive');
+
+var sun = s.circle(400, 300, 20);
+sun.attr({
+  fill : 'yellow'
+});
+
+var planets = {};
+
+function getPlanet(url, name) {
+  if(typeof url === 'object') {
+    name = url.name;
+    url = url.url;
+  }
+  Snap.load(url, function(f) {
+    var g = f.select('g');
+    s.append(g);
+    planets[name] = g;
+  });
+}
+
+// getPlanet('../images/planets_Earth.svg', 'earth');
+
+[
+  {url : '../images/planets_Mercury.svg', name : 'mercury'},
+  {url : '../images/planets_Venus.svg', name : 'venus'},
+  {url : '../images/planets_Earth.svg', name : 'earth'},
+  {url : '../images/planets_Mars.svg', name : 'mars'},
+  {url : '../images/planets_Jupiter.svg', name : 'jupiter'},
+  {url : '../images/planets_Saturn.svg', name : 'saturn'},
+  {url : '../images/planets_Uranus.svg', name : 'uranus'},
+  {url : '../images/planets_Neptune.svg', name : 'neptune'}
+].map(getPlanet);
+
+// Snap.load('../images/planets_Earth.svg', function(f) {
+//   var g = f.select('g');
+//   s.append(g);
+// });
